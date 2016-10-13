326b00c25c14e3fdff66f87f0bd5bd1735b318da
caseydwayne <caseydwayne@creativedesigninfluence.com>
2016-10-13 04:26:46 -0400

    Signed-off-by: caseydwayne <caseydwayne@creativedesigninfluence.com>

--- index.js
+++ index.js
@@ -1,39 +1,129 @@
-(function(){
+module.exports = (function(DEBUG){
+/*---------------------------------------------------------------------*/
+
+  //setup the vitals
+  
+  var root = this;
+  var defined = require('dsb-defined');
+  var toString = require('dsb-to-string');  
+  var rx = require('dsb-rx/objectType');
+  var lowercase = function(v){
+    return String.prototype.toLowerCase.call( v );
+  };  
+  
+/***********************************************************************/
 
   /* @method typecheck
    * returns the real type of an object
-   * @param {any} check object to check
-   * @return {string} truetype (object, array, regexp, ...)
-   * @native
+   * @param check {any} object to check
+   * @param [against] {string} string to check type against (via contains())
+   * @param [strict] {boolean} if false, will do a shallow typecheck (may not be true type, but useful for some scenarios)
+   * @return truetype {string} i.e., object, array, regexp, ...
+   * @native supported
    */
   
-  
-  var lowercase = function(v){ return String.prototype.toLowerCase.call( v || this ); };
-  var defined = require('dsb-defined');
-  var toString = require('dsb-to-string');
-  var root = this;
-  var rx = /\[\w+ (.*)\]/;
-  
-  
-  var typecheck = function(check, extra){ // advanced type-checking made easy
-    var x, c, u = 'undefined';
-    if( defined( this ) ){
-      x = this; c = check;//, u = true;
-    } else {
-      x = check; c = extra || false;
-    }
-    //if( c === 'undefined' && u ) return true;
-    var t, m, r, f;
-    t = toString( x ); //Object.prototype.toString.call(     
-    if( t === toString(root) ) return c ? c === u : u;
-    m = t.match( rx );
-    r = m.length > 1 ? lowercase( m[1] ) : t;
+/*---------------------------------------------------------------------*/
     
-    f = typeof c === 'string' ? r.indexOf( lowercase( c ) ) >-1 : r;
-    //alert( 'Found '+t+' | stripped: '+r+' | check: '+c+' contains? '+f ); 
-    return f;
-  };  
   
-  return module.exports = typecheck;
+  var typecheck = function(check, against, strict){ // advanced type-checking made easy
+    
+    //setup
+    var c, a, s = true, u = 'undefined';
 
-}());
+/*---------------------------------------------------------------------*/
+
+    var resolve = function(){
+      
+      var _c = defined( check ), 
+          _a = defined( against ),
+          _s = defined( strict );
+      
+      if(DEBUG){
+        console.log( { cd: _c, ad: _a, sd: _s } );
+      }
+      
+      
+      //check for full
+      if( _s ){
+        if( typeof strict === 'boolean' ){
+          c = check;
+          a = against;
+          s = strict;
+          return "typecheck( check, against, strict )";
+        }
+      }
+      //make sure something is there to check
+      if( _c  ){
+        //assign check
+        c = check;
+        //resolve against/strict
+        if( _a ){
+          //check for standard
+          if( typeof against === 'string' ){
+            a = against;
+            return "typecheck( check, against )";
+          }          
+          //check for typecheck(soft)
+          if( typeof against === 'boolean' ){
+            s = against;
+            return "typecheck( check, strict )";
+          }          
+        }
+        return "typecheck( check )";
+      }      
+    };
+    
+    var signature = resolve();
+
+/*---------------------------------------------------------------------*/
+
+    //the business logic
+    
+    //setup
+    var t, m, r, f;
+    
+    //Object.prototype.toString.call by default
+    t = toString( c ); 
+    
+    //if not strict, try to do a soft {toString} conversion
+    if( !s ) try { t = c.toString(); } catch (e){};
+    
+    //return undefined if {check} is root object
+    if( t === toString(root) ){      
+      return a ? a === u : u;
+    }
+    
+    //attempt to match
+    m = t.match( rx );
+    
+    //register match or set {r} as type string
+    r = m && m.length > 1 ? lowercase( m[1] ) : t;    
+    //console.log(r);
+    //if against is a string, check against check's {r}
+    f = (typeof a === 'string') ? r.indexOf( lowercase( a ) ) >-1 : r;    
+    
+/*---------------------------------------------------------------------*/
+
+    if( DEBUG ){
+      console.log('\n-----------------------');
+      
+      console.log('Calling '+signature );
+      var to = { check: c, against: a, strict: s, returning: f };
+      console.log( require('dsb-printr')( to ) );
+      console.log('-----------------------\n');
+    }
+    
+/*---------------------------------------------------------------------*/
+
+    return f;
+
+/*---------------------------------------------------------------------*/
+    
+  };//end typecheck
+  
+/***********************************************************************/  
+
+  return typecheck;
+
+/*---------------------------------------------------------------------*/
+}(false));
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-(function(){
-  
+module.exports = (function(){
+/*---------------------------------------------------------------------*/
+
   /*
    * typecheck test
    */
@@ -9,6 +10,12 @@
            each = require('dsb-each');        
   
   
+  var obj = {
+    aval: 'value',
+    typecheck: typecheck
+  };
+  
+/*---------------------------------------------------------------------*/  
   
   /*    
    * typecheck
@@ -18,19 +25,35 @@
     var p = debug.presets,
         u = 'undefined',
        _u,
+       
         // type = string of what it should return
         // value = item to check against
         // callit = boolean to also test as 'this' method
         t = function(type, value, callit){
-          var c = callit !== void 0 ? callit : true;                      
-          test( type, fn( value ), type );
+          
+          var c = callit !== void 0 ? callit : true;
+          
+          //standard tests          
+          var t = fn( value );
+          test( type, t, type );
+          
+          //run inside another object
+          var ti = obj[name]( value );
+          test( type, ti, type );
+          
           if( c ){
+            var ok;
             try {
-              test( type+' *call', fn.call( value, type ), true );
+              ok = fn.call( value, type );
             }
-            catch(error){}
+            catch(error){
+              debug.log( 'Native support is unavailable on '+name );
+            }
+            //console.log('ok returning ',ok);
+            //if( ok ) test( type+' *call', ok, true );
           }
         };
+        //console.log(p);
     //test each type of preset against each other
     each(p, function( v, k ){ t( k, v ); });
     //test undefined
@@ -38,5 +61,10 @@
   }, typecheck );
  
    debug.complete();
-  /**/        
+
+/*---------------------------------------------------------------------*/
+  
+  return debug;
+
+/*---------------------------------------------------------------------*/  
 }());
